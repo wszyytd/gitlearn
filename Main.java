35b7bcdebce1d0f86c3164b9303f8819fbd8d6a4
sunbo <issunbo@qq.com>
2026-07-10 21:33:25 +0800

    array

--- Main.java
+++ Main.java
@@ -1,5 +1,10 @@
-public class Main{
+import java.util.Arrays;
+
+public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        int[] array = { 1, 2, 3, 4 };
+        System.out.println(Arrays.toString(array));
     }
 }
