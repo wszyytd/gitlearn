e464a148d8ff5ebae96dd527c2073c28d544ebd3
sunbo <issunbo@qq.com>
2026-07-10 21:58:19 +0800

    Person

---
+++ Person.java
@@ -0,0 +1,23 @@
+public class Person implements Comparable<Person> {
+    private int id;
+    private String name;
+
+    public Person(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public int compareTo(Person o) {
+        return Integer.compare(this.id, o.id);
+    }
+
+}
